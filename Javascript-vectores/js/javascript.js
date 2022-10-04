75cb6a6ebe0a07ec15de2f1e5e09c8542ec54338
Rodritz <rodrigourq@gmail.com>
2022-10-03 21:20:43 -0300

    cambios

--- Javascript-vectores/js/javascript.js
+++ Javascript-vectores/js/javascript.js
@@ -72,7 +72,7 @@ for(let amigo of amigos){
 }
 console.log('menores de 40', menoresDe40);
 
-//listado de amigos con map
+//listado de amigos con funcion lambda map
 const amigotes = amigos.map(amigo=>amigo.nombre);
 console.log('amigotes', amigotes);
 
@@ -94,6 +94,100 @@ console.log('hay algun menor de 30', menorDe30)
 
 const mayoresDe25 = amigos.every(amigo=>amigo.edad>25);
 console.log('son todos mayores de 25', mayoresDe25);
+
+
+//nueva practica
+
+const alumnos = [
+    {
+        nombre: 'Juan',
+        edad:20,
+    },
+    {
+        nombre: 'pedro',
+        edad:20,
+    },
+    {
+        nombre:'robert',
+        edad:20,
+    },
+    {
+        nombre: 'marcos',
+        edad:20,
+    },
+
+]
+sumaEdades = 0;
+//formas de recorrer un vector alumnos
+let mayores; //t|f
+for(let i=0; i<alumnos.length; i++){
+    mayores = alumnos[i].edad>40;
+}
+console.log('alumnos mayores', mayores);
+
+for(let i=0; i<alumnos.length; i++){
+	sumaEdades += alumnos[i].edad
+}
+console.log('edades', sumaEdades);
+
+sumaEdades = 0;
+let menores; //t|f
+for(let alumno of alumnos){
+	menores = alumno.edad<30;
+}
+console.log('alumnos menores', menores);
+
+sumaEdades = 0;
+for(let alumno of alumnos){
+	sumaEdades += alumno.edad;
+}
+console.log('edades', sumaEdades);
+
+sumaEdades = 0;
+//funcion lambda array.forEach
+alumnos.forEach(alumno=>sumaEdades += alumno.edad);
+console.log('edades funcion forEach', sumaEdades);
+
+//funcion lambda reduce
+const sumaTotal = alumnos.reduce((acum, actual)=>acum + actual.edad, 0);
+console.log('edades funcion reduce', sumaTotal);
+
+//funcion lambda filter
+const alumnosMenores = alumnos.filter(alumno=>alumno.edad <40);
+console.log('menores de 40 funcion filter', alumnosMenores);
+
+//funcion lambda every(t|f)
+const todos = alumnos.every(alumno=>alumno.edad ===35);
+console.log('tienen todos 35?', todos);
+
+//funcion lambda map
+const alumnosNuevos = alumnos.map(alumno=>alumno.nombre);
+console.log('amigos funcion map', alumnos);
+
+//funcion lambda some (t|f)
+const menorDe25 = alumnos.some(alumno=>alumno.edad<25);
+console.log('hay algun menor de 25?', menorDe25);
+
+//mover de un vector a otro
+const jovenesAlumnos = [];
+for(let alumno of alumnos){
+	if(alumno.edad <40)
+		jovenesAlumnos.push(alumno);
+}
+console.log('jovenes alumnos', jovenesAlumnos)
+	
+	
+
+
+
+
+
+
+
+
+
+
+
 
 
 
